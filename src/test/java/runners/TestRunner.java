2b03a1a3ed7fdbed0fd06efd25894e50d5c33e74
Ayberk Gürfidan <ayberk546@gmail.com>
2022-06-08 23:16:27 +0300

    08.06.2022

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,13 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-reports.html"},
+        plugin = {"html:target/cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@guru",
+        tags = "@parametre",
         dryRun = false
 )
-public class Runner {
+public class TestRunner {
     /*
     Bir frameworkte bir tek Runner Class'i yeterli olabilir.
     Runner class'inda hic bir sey olmaz
--- src/test/resources/features/US1003_parametreKullanimi.feature
+++ src/test/resources/features/US1003_parametreKullanimi.feature
@@ -3,6 +3,6 @@ Feature: US1003 kullanici parametre kullanarak arama yapabilmeli
   @parametre
   Scenario: TC06_ kullanici parametre ile amazonda arama yapabilmeli
     Given kullanici amazon anasayfasinda
-    And kullanici "Nutella" icin arama yapar
-    And sonucun "Nutella" icerdigini test eder
+    And kullanici "Java" icin arama yapar
+    And sonucun "Java" icerdigini test eder
     Then sayfayi kapatir
